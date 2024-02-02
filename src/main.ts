beb422433bcf1b1eabe31d3d3fa59f6c2b0a35da
Zhenkaaf <zhenkaaf@gmail.com>
2024-02-02 10:16:19 +0200

    test8

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { NestFactory } from '@nestjs/core';
+/* import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -11,4 +11,28 @@ async function bootstrap() {
     console.log(`*************************Server is running on port ${port}`);
   });
 }
+bootstrap(); */
+
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  try {
+    const app = await NestFactory.create(AppModule);
+    app.setGlobalPrefix('api');
+    app.enableCors();
+
+    const port = process.env.PORT || 3000;
+
+    await app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.error(
+      'Error********************* starting the Nest application:',
+      error,
+    );
+  }
+}
+
 bootstrap();
