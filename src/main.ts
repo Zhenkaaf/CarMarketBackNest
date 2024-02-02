0d901806c92c1375c868f95dd0833b6565ad14a0
Zhenkaaf <zhenkaaf@gmail.com>
2024-02-02 09:20:47 +0200

    test7

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
 
   const port = process.env.PORT || 3000;
   await app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`*************************Server is running on port ${port}`);
   });
 }
 bootstrap();
