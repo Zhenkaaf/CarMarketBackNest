b44ddc17b060889d6592b464cc6b1a9d61f6dff1
Zhenkaaf <zhenkaaf@gmail.com>
2024-02-02 12:59:58 +0200

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,19 +16,17 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm/dist/interfaces/typeorm-op
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => {
         const logger = new Logger('Database');
-        logger.log(`DB_HOST: ${configService.get('DB_HOST')}`);
-        logger.log(`DB_PORT: ${configService.get('DB_PORT')}`);
-        logger.log(`DB_USERNAME: ${configService.get('DB_USERNAME')}`);
-        logger.log(`DB_PASSWORD: ${configService.get('DB_PASSWORD')}`);
-        logger.log(`DB_NAME: ${configService.get('DB_NAME')}`);
+        logger.log(`POSTGRES_URL: ${configService.get('POSTGRES_URL')}`);
 
+        const databaseUrl = configService.get('POSTGRES_URL');
         const databaseConfig: TypeOrmModuleOptions = {
           type: 'postgres',
-          host: configService.get('DB_HOST'),
+          /* host: configService.get('DB_HOST'),
           port: configService.get('DB_PORT'),
           username: configService.get('DB_USERNAME'),
           password: configService.get('DB_PASSWORD'),
-          database: configService.get('DB_NAME'),
+          database: configService.get('DB_NAME'), */
+          url: databaseUrl,
           synchronize: true,
           entities: [__dirname + '/**/*.entity{.js, .ts}'],
         };
