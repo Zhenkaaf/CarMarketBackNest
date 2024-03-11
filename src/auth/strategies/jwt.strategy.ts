656cea92b86efdff15ee5dac87fa59219f581755
Zhenkaaf <zhenkaaf@gmail.com>
2024-03-11 12:09:43 +0200

    JwtStrategyValidateUpdate

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -3,10 +3,14 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 import { IUser } from 'src/types/types';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private readonly configService: ConfigService) {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly userService: UserService,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
@@ -14,12 +18,17 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  async validate(user: IUser) {
+  async validate(dataFromToken: IUser) {
+    console.log('dataFromToken***', dataFromToken);
+    const user = await this.userService.findOne(dataFromToken.email);
+    console.log('user***', user);
     return {
-      id: user.id,
-      email: user.email,
+      id: dataFromToken.id,
+      email: dataFromToken.email,
       phoneNumber: user.phoneNumber,
       name: user.name,
+      createdAt: user.createdAt,
     };
   }
 }
+/* При использовании стратегии JWT аутентификации, библиотека Passport автоматически извлекает данные из токена и передает их в функцию validate */
