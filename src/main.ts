ce31664de257c96c0bc0c47a5fe1976cfd5c9cbd
Zhenkaaf <zhenkaaf@gmail.com>
2024-03-18 14:53:02 +0200

    test

--- src/main.ts
+++ src/main.ts
@@ -5,23 +5,6 @@ async function bootstrap() {
   try {
     const app = await NestFactory.create(AppModule);
     app.setGlobalPrefix('api');
-    // Добавляем middleware для обработки OPTIONS запросов
-    app.use((req, res, next) => {
-      res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
-      res.header(
-        'Access-Control-Allow-Methods',
-        'GET, PUT, POST, DELETE, OPTIONS',
-      );
-      res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-      );
-      if (req.method === 'OPTIONS') {
-        res.sendStatus(200);
-      } else {
-        next();
-      }
-    });
 
     app.enableCors();
 
