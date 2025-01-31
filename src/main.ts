fdd7ec7aea593d539d19589887c5c3860427ec83
Zhenkaaf <zhenkaaf@gmail.com>
2025-01-31 16:53:56 +0200

    OPTIONS

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
 
     app.enableCors({
       origin: ['https://zhenkaaf.github.io'],
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
       allowedHeaders: 'Content-Type, Authorization',
       credentials: true,
     });
